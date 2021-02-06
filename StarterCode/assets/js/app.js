b127a9976f0f8aa539be4e96be33059dbeb9d75e
iastanehasl <iastanehasl3@gmail.com>
2021-02-06 10:22:04 -0500

    adding final code

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,129 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight)
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`)
+  .attr("class", "chart");
+
+// Import Data
+d3.csv("data.csv").then(function(healthData) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    healthData.forEach(function(data) {
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+    });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([(d3.min(healthData, d => d.poverty)* 0.9),d3.max(healthData, d => d.poverty)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([(d3.min(healthData, d => d.healthcare)* 0.8), d3.max(healthData, d => d.healthcare)])
+      .range([height, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var xAxis = d3.axisBottom(xLinearScale);
+    var yAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis);
+
+    chartGroup.append("g")
+      .call(yAxis);
+
+    // Create axes labels
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "aText")
+    .text("% Lacks Healthcare");
+
+    chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "aText")
+    .text("% in Poverty");
+    
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+      .data(healthData)
+      .enter()
+      .append("circle")
+      .attr("class", "stateCircle")
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcare))
+      .attr("r", 15)
+    //   .attr("stroke-width", "1")
+    //   .attr("stroke", "black")
+    //   .attr("fill", "lightblue")
+    //   .attr("opacity", "1");
+
+    var circleLabels = chartGroup.selectAll(null).data(healthData).enter().append("text");
+
+    circleLabels
+        .attr("x", function(d) {
+            return xLinearScale(d.poverty);
+        })
+        .attr("y", function(d) {
+            return yLinearScale(d.healthcare);
+        })
+        .text(function(d) {
+            return d.abbr;
+        })
+        .attr("class", "stateText");
+
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([80, -60])
+        .html(function(d) {
+            return (`<strong>${d.state}</strong><br>% in Poverty: ${d.poverty}<br>% Lacks Healthcare: ${d.healthcare}`);
+        });
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    circlesGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data);
+      })
+        // onmouseout event
+        .on("mouseout", function(data, index) {
+          toolTip.hide(data);
+        })
+    
+      return circlesGroup;
+      
+}).catch(function(error) {
+     console.log(error);
+});
